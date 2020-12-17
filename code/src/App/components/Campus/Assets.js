0eaae335e3098d98e7fede9e7e7344dae66bfb5e
Ruben <maesfranckxruben@gmail.com>
2020-12-17 23:25:19 +0100

    Update Assets.js

--- code/src/App/components/Campus/Assets.js
+++ code/src/App/components/Campus/Assets.js
@@ -1,12 +1,9 @@
 import BruineZitbank from "../jeroen/BruineZitbank";
 import ZwarteZitbank from "../jeroen/ZwarteZitbank";
 import LavaboAndMirror from "../jeroen/LavaboAndMirror";
-<<<<<<< HEAD
 
-=======
 import Whiteboard from "../jeroen/Whiteboard";
 import Beamer from "../jeroen/Beamer";
->>>>>>> 035ed92bf38ed783a5beb5ea6a8289519c59ff78
 import Bookshelve from "../indy/bookshelves";
 import StopcontactMediatheek from "../indy/stopcontactMediatheek";
 import TafelMediatheek from "../indy/tafelmediatheek";
@@ -15,8 +12,6 @@ import Imac from "../indy/imac";
 import Windows from "../indy/windows";
 import Scanners from "../indy/scanners";
 import Rondestoel from "../indy/rondestoel";
-<<<<<<< HEAD
-=======
 import MetalKast from "../indy/kabinetKast";
 
 
@@ -28,7 +23,6 @@ import Plant from "../ruben/plant";
 import SeatGrid from "../jonas/SeatGrid";
 import OvaleTafels from "../jonas/OvaleTafels";
 import GroteTafel from "../jonas/GroteTafel";
->>>>>>> 035ed92bf38ed783a5beb5ea6a8289519c59ff78
 
 import Tafel_eetzaal from "../morgane/Tafels_eetzaal";
 import Kickertafel from "../morgane/Kickertafel_eetzaal";
