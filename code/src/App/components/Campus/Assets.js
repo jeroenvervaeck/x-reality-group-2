416120efc90956b411eeb34833d3c2cc0f9c1447
pgmgent-{MorgBeka} <{morgbeka@student.arteveldehs.be}>
2020-12-18 09:59:31 +0100

    wip

--- code/src/App/components/Campus/Assets.js
+++ code/src/App/components/Campus/Assets.js
@@ -1,12 +1,10 @@
 import BruineZitbank from "../jeroen/BruineZitbank";
 import ZwarteZitbank from "../jeroen/ZwarteZitbank";
 import LavaboAndMirror from "../jeroen/LavaboAndMirror";
-<<<<<<< HEAD
 
-=======
 import Whiteboard from "../jeroen/Whiteboard";
 import Beamer from "../jeroen/Beamer";
->>>>>>> 035ed92bf38ed783a5beb5ea6a8289519c59ff78
+
 import Bookshelve from "../indy/bookshelves";
 import StopcontactMediatheek from "../indy/stopcontactMediatheek";
 import TafelMediatheek from "../indy/tafelmediatheek";
@@ -15,20 +13,13 @@ import Imac from "../indy/imac";
 import Windows from "../indy/windows";
 import Scanners from "../indy/scanners";
 import Rondestoel from "../indy/rondestoel";
-<<<<<<< HEAD
-=======
+
 import MetalKast from "../indy/kabinetKast";
-
-
-
-import Tafel_eetzaal from "../morgane/Tafels_eetzaal";
-
 
 import Plant from "../ruben/plant";
 import SeatGrid from "../jonas/SeatGrid";
 import OvaleTafels from "../jonas/OvaleTafels";
 import GroteTafel from "../jonas/GroteTafel";
->>>>>>> 035ed92bf38ed783a5beb5ea6a8289519c59ff78
 
 import Tafel_eetzaal from "../morgane/Tafels_eetzaal";
 import Kickertafel from "../morgane/Kickertafel_eetzaal";
@@ -36,9 +27,6 @@ import Snackautomaat from "../morgane/Snackautomaat_eetzaal";
 import Koffieautomaat from "../morgane/Koffieautomaat_eetzaal";
 import Brandslang from "../morgane/Brandslang_eetzaal";
 import Vuilbakken from "../morgane/Vuilbakken_eetzaal";
-
-
-import Plant from "../ruben/plant";
 
 
 export default (props) => {
