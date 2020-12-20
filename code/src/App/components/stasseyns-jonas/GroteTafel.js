e2755ccaea5d987d236d7162c368c5836e676939
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2020-12-20 17:21:57 +0100

    Positioning of objects in B14

--- code/src/App/components/stasseyns-jonas/GroteTafel.js
+++ code/src/App/components/stasseyns-jonas/GroteTafel.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import {useGLTF, useMatcapTexture} from "@react-three/drei";
 
 import modelPath from "./models/GroteTafel.glb";
 
@@ -8,13 +8,13 @@ export default () => {
 
     console.log("Nodes:", nodes);
     // console.log("Materials:", materials);
-
+    //796D6B_DED3CB_C6BAB1_ADA09B
     return (
         <group>
             <primitive // Big Table B14
                 object={nodes.Scene.clone()}
                 // scale={[0.05, 1, 0.05]}
-                position={[-8, 1, 1]}
+                position={[-8, 0.3, 0.95]}
                 material={materials["Material.001"]}
                 castShadow={true}
             />
