9ed47d0ec9e8f6d8698d0e9cbfd10bbafc3c17b8
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2020-12-20 15:22:34 +0100

    1st floor Toilet Stalls completed

--- code/src/App/components/stasseyns-jonas/ToiletStall.js
+++ code/src/App/components/stasseyns-jonas/ToiletStall.js
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import React from "react";
+import {useGLTF} from "@react-three/drei";
 
 import modelPath from "./models/Toilet.glb";
 
@@ -43,11 +43,51 @@ export default () => {
                 material={materials["Material.004"]}
                 castShadow={true}
             />
-            <mesh // Left Toilet + Flush
+            <mesh // Left Toilet
                 geometry={nodes.Toilet.children[1].geometry}
-                scale={[0.6, 1, 0.01]}
+                scale={[0.7, 1, 0.01]}
                 rotation={[0, 0, 0]}
-                position={[-8, 1.3, -7.6]}
+                position={[-8.6, 1.3, -7.7]}
+                material={materials["Material.004"]}
+                castShadow={true}
+            />
+            <mesh // Right Toilet
+                geometry={nodes.Toilet.children[1].geometry}
+                scale={[0.7, 1, 0.01]}
+                rotation={[0, 0, 0]}
+                position={[-8.6, 1.3, -9]}
+                material={materials["Material.004"]}
+                castShadow={true}
+            />
+            <mesh // Left Flush Buttons
+                geometry={nodes.Flush.children[0].geometry}
+                scale={[1, 1, 1]}
+                rotation={[0, 0, -1.5707]}
+                position={[-9.3, 1.3, -8.275]}
+                material={materials["Button"]}
+                castShadow={true}
+            />
+            <mesh // Left Flush
+                geometry={nodes.Flush.children[1].geometry}
+                scale={[1, 1, 1]}
+                rotation={[0, 0, -1.5707]}
+                position={[-9.3, 1.3, -8.275]}
+                material={materials["Material.004"]}
+                castShadow={true}
+            />
+            <mesh // Right Flush Buttons
+                geometry={nodes.Flush.children[0].geometry}
+                scale={[1, 1, 1]}
+                rotation={[0, 0, -1.5707]}
+                position={[-9.3, 1.3, -9.575]}
+                material={materials["Button"]}
+                castShadow={true}
+            />
+            <mesh // Right Flush
+                geometry={nodes.Flush.children[1].geometry}
+                scale={[1, 1, 1]}
+                rotation={[0, 0, -1.5707]}
+                position={[-9.3, 1.3, -9.575]}
                 material={materials["Material.004"]}
                 castShadow={true}
             />
